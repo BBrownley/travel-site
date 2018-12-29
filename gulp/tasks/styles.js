6724534a18f5f4cb280988ccfdf0337435168606
BBrownley <brendobrownley@yahoo.com>
2018-12-28 22:05:42 -0500

    Lesson 19 - Mobile-first with PostCSS mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,8 +4,10 @@ const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const cssImport = require('postcss-import');
+const mixins = require("postcss-mixins");
 
 gulp.task("styles", function() {
+  
   //enter gulp css to use this task
 
   //Be careful how these lines are spaced out - It could cause errors
@@ -13,7 +15,7 @@ gulp.task("styles", function() {
   //cssImport should be the first task
 
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, cssvars, autoprefixer, nested]))
+  .pipe(postcss([cssImport, mixins, cssvars, autoprefixer, nested]))
 
   //We must throw an error before the function ends
 
